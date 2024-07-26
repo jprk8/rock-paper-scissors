e1b5cd9acc01dc3170ebad85915f89410ffa51d9
John Park <176120986+jprk8@users.noreply.github.com>
2024-07-26 00:59:37 -0700

    Make a function for random computer choice

---
+++ script.js
@@ -0,0 +1,14 @@
+function getComputerChoice() {
+    choice = Math.floor(Math.random() * 3);
+    if (choice == 0) {
+        return "rock";
+    } else if (choice == 1) {
+        return "paper";
+    } else if (choice == 2) {
+        return "scissors";
+    }
+}
+
+function getHumanChoice() {
+    
+}
